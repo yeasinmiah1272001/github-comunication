7c5ff1aa58c171f0040fa7634187457809d4aefb
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-07 00:53:10 +0600

    ssd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='mx-10'>
+    <div className=''>
      <h1>Yeasin Ahmed</h1>
      <p>I am a Junior Frontend Developer with a strong foundation in HTML, CSS, and React.js. I specialize in creating interactive and user-friendly web applications, and I'm currently expanding my skills in Next.js and TypeScript. Passionate about seamless user experiences and web performance, I use Tailwind CSS for responsive design and integrate various libraries to enhance functionality. I stay updated with industry trends to deliver modern, cutting-edge solutions.</p>
      <p>I am a Junior Frontend Developer with a strong foundation in HTML, CSS, and React.js. I specialize in creating interactive and user-friendly web applications, and I'm currently expanding my skills in Next.js and TypeScript. Passionate about seamless user experiences and web performance, I use Tailwind CSS for responsive design and integrate various libraries to enhance functionality. I stay updated with industry trends to deliver modern, cutting-edge solutions.</p>
      <p>I am a Junior Frontend Developer with a strong foundation in HTML, CSS, and React.js. I specialize in creating interactive and user-friendly web applications, and I'm currently expanding my skills in Next.js and TypeScript. Passionate about seamless user experiences and web performance, I use Tailwind CSS for responsive design and integrate various libraries to enhance functionality. I stay updated with industry trends to deliver modern, cutting-edge solutions.</p>
+     <h1>helo gelo</h1>
       </div>
   )
 }
