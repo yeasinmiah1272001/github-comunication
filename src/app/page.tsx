7942fbdd70fedc81889f17f71097379d22d99529
shahinalom <shahinalom3511371@gmail.com>
2024-09-07 01:01:55 +0600

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,9 @@ const HomePage = () => {
      <p>I am a Junior Frontend Developer with a strong foundation in HTML, CSS, and React.js. I specialize in creating interactive and user-friendly web applications, and I'm currently expanding my skills in Next.js and TypeScript. Passionate about seamless user experiences and web performance, I use Tailwind CSS for responsive design and integrate various libraries to enhance functionality. I stay updated with industry trends to deliver modern, cutting-edge solutions.</p>
      <h1>helo gelo</h1>
      <h1>Md Shahin Alom</h1>
+     <h1>Md Shahin Alom</h1>
+     <h1>Md Shahin Alom</h1>
+     <h1>Md Shahin Alom</h1>
       </div>
   )
 }
